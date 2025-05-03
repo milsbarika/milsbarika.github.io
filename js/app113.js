70455c860a78c585b5ed8274678d0dc8c1402e05
Moussa <155907239+milsbarika@users.noreply.github.com>
2025-05-03 21:09:40 +0100

    Add files via upload

---
+++ js/app113.js
@@ -0,0 +1,11 @@
+﻿
+	
+	$( document ).ready(function(){	
+		$('#menu1 li a').click(function(){
+			$('#menu1 a').removeClass('active');             // utiliser cette methode ou l'autre
+			$(this).addClass('active');
+					
+			/* $(this).addClass('active').parent().siblings().find('a').removeClass('active'); */
+		 });
+		 		 
+	 });	 
